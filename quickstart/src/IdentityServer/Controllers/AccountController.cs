b467f60716392c9f414c03df4d3326686c8b7467
ovata <ovataworld@gmail.com>
2020-06-05 08:47:37 +0100

    Initial commit

---
+++ quickstart/src/IdentityServer/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IdentityServer.Controllers
+{
+    [Controller]
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost]
+        public IActionResult Register([FromBody] RegistrationViewModel viewModel)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ quickstart/src/IdentityServer/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.ViewModel
+{
+    public class RegistrationViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
